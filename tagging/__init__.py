2bec25a37ace19e359ef19047910aee956f203ba
Roman Telezhynskyi <dismine@gmail.com>
2022-12-21 17:02:58 +0200

    New version.

--- tagging/__init__.py
+++ tagging/__init__.py
@@ -1,15 +1,13 @@
 """
 Django-tagging
 """
-__version__ = '0.5.0'
+__version__ = '0.5.0+dismine.1'
 __license__ = 'BSD License'
 
 __author__ = 'Jonathan Buchanan'
 __author_email__ = 'jonathan.buchanan@gmail.com'
 
-__maintainer__ = 'Fantomas42'
-__maintainer_email__ = 'fantomas42@gmail.com'
+__maintainer__ = 'dismine'
+__maintainer_email__ = 'dismine@gmail.com'
 
-__url__ = 'https://github.com/Fantomas42/django-tagging'
-
-default_app_config = 'tagging.apps.TaggingConfig'
+__url__ = 'https://github.com/dismine/django-tagging'
